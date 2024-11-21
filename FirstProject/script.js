60eb3801f25958bb2446fddc4267029fae5b5ad8
Ujjawal <ujjawalpra@gmail.com>
2024-11-21 23:56:14 +0545

    First scene setup completed

--- FirstProject/script.js
+++ FirstProject/script.js
@@ -1,2 +1,37 @@
 import * as THREE from "three"
-console.log(THREE)
+
+const canvas = document.querySelector('canvas.webgl')
+
+
+console.log(canvas)
+//scene
+const scene = new THREE.Scene()
+
+//Geometry or mesh
+const geom = new THREE.BoxGeometry(1,1,1);
+const mat = new THREE.MeshBasicMaterial({
+    color: 0xff0000
+})
+const mesh = new THREE.Mesh(geom,mat)
+scene.add(mesh);
+
+const size = 
+{
+    width : 800,
+    height : 600
+}
+
+
+//camera
+const cam = new THREE.PerspectiveCamera(60,size.width/size.height);
+cam.position.z = 3
+scene.add(cam)
+
+
+//renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+
+renderer.setSize(size.width, size.height);
+renderer.render(scene,cam)
